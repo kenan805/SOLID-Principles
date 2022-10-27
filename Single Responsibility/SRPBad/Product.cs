54248b4d52a767213edabcbda2dd52591af0dc97
Kənan İdayatov <idayatovkenan7@gmail.com>
2022-10-27 17:05:27 +0400

    Single Responsibility added

---
+++ Single Responsibility/SRPBad/Product.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility.SRPBad
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        private static List<Product> productList = new List<Product>();
+
+        public Product()
+        {
+            productList = new()
+            {
+                new(){ Id=1, Name= "Book" },
+                new(){ Id=2, Name= "Pen" },
+                new(){ Id=3, Name= "Phone" },
+                new(){ Id=4, Name= "Computer" },
+                new(){ Id=5, Name= "Bag" },
+            };
+        }
+
+        public List<Product> GetProducts() => productList;
+
+        public void SaveOrUpdate(Product product)
+        {
+            var hasProduct = productList.Any(p => p.Id == product.Id);
+            if (!hasProduct)
+            {
+                productList.Add(product);
+            }
+            else
+            {
+                var index = productList.FindIndex(p => p.Id == product.Id);
+                productList[index] = product;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var hasProduct = productList.Find(p => p.Id == id);
+
+            if (hasProduct == null)
+            {
+                throw new Exception("Product is null");
+            }
+
+            productList.Remove(hasProduct);
+        }
+
+        public void WriteToConsole()
+        {
+            productList.ForEach(p =>
+            {
+                Console.WriteLine($"{p.Id} - {p.Name}");
+            });
+        }
+    }
+}
---
+++ Single Responsibility/SRPGood/ProductPresenter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Single_Responsibility.SRPGood
+{
+    public class ProductPresenter
+    {
+        public void WriteToConsole(List<Product> productList)
+        {
+            productList.ForEach(p =>
+            {
+                Console.WriteLine($"{p.Id} - {p.Name}");
+            });
+        }
+    }
+}
